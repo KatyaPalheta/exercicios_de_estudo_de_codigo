f97a0c549ec17d96e1c90fb8b4a5a1b0a76568ba
KATYA SYLENE FONTENELLE PALHETA <109077065+KatyaPalheta@users.noreply.github.com>
2022-11-05 17:20:16 -0300

    estudos front-end ✏️

--- alura_consumo_api_javascript/script.js
+++ alura_consumo_api_javascript/script.js
@@ -1,14 +1,32 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
+	var mensagemErro = document.getElementById('erro');
+	mensagemErro.innerHTML = '';
 	try {
-		var consultaCEP = await fetch('https://viacep.com.br/ws/01001300/json/');
+		var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`); //concatecação de variavel dentro do valor
 		var consultaCepConvertida = await consultaCEP.json();
 		if (consultaCepConvertida.erro) {
 			throw Error('CEP não existente!');
 		}
+		var cidade = document.getElementById('cidade');
+		var logradouro = document.getElementById('endereco');
+		var bairro = document.getElementById('bairro');
+		var estado = document.getElementById('estado');
+
+		cidade.value = consultaCepConvertida.localidade;
+		logradouro.value = consultaCepConvertida.logradouro;
+		bairro.value = consultaCepConvertida.bairro;
+		estado.value = consultaCepConvertida.uf;
+
 		console.log(consultaCepConvertida);
+
+		return consultaCepConvertida;
 	} catch (erro) {
+		mensagemErro.innerHTML = `<p>CEP inválido.Tente novamente!</p>`;
 		console.log(erro);
 	}
 }
 
 buscaEndereco();
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
